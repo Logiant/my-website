86bbe54f4272056cd8bc09a381d2ab312d85be3e
Logan <Logiant@users.noreply.github.com>
2023-06-29 14:31:41 -0400

    added ODU

--- js/updateText.js
+++ js/updateText.js
@@ -1,5 +1,5 @@
-var roles = ["engineer", "scientist", "postdoc", "researcher"];
-var roles_count = 0;
+//var roles = ["engineer", "scientist", "postdoc", "researcher"];
+//var roles_count = 0;
 
 var topics = ["cyber-physical", "emergent", "bio-inspired", "large-scale", "robotic", "swarm"];
 var topics_count = 0;
@@ -7,10 +7,10 @@ var topics_count = 0;
 delay = 4500;
 
 function changeRoleText() {
-    $("#role").fadeOut(1000, function() {
-        $(this).text(roles[roles_count]).fadeIn(500);
-    });
-    roles_count < 4 ? roles_count++ : roles_count = 0;
+//    $("#role").fadeOut(1000, function() {
+//        $(this).text(roles[roles_count]).fadeIn(500);
+//    });
+//    roles_count < 4 ? roles_count++ : roles_count = 0;
     setTimeout(changeTopicText, delay);
 }
 
